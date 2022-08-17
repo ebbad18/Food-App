f5c1ecc00a135a3ba19d4978f21e219747eda4ab
ebbad18 <ebbadsiddique@gmail.com>
2022-08-18 02:01:11 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Category from './components/Category'
+import Food from './components/Food'
+import HeadlineCard from './components/HeadlineCard'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero />
+      <HeadlineCard />
+      <Food />
+      <Category />
+    </div>
+  )
+}
+
+export default App
